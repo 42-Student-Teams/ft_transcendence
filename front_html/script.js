2aa73841cb939623935c24cd68b965e377f9b008
Neila <neilamumi@hotmail.fr>
2024-06-08 20:34:46 +0200

    chat et dashboard html/css added mais pas parfait

--- front_html/script.js
+++ front_html/script.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Le DOM est chargé');
+document.addEventListener("DOMContentLoaded", function() {
+    var ctx = document.getElementById('chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Body hits', 'Leg hits', 'Head hits'],
+            datasets: [{
+                label: 'Shot Distribution',
+                data: [30.12, 50.12, 20.12],
+                backgroundColor: ['#28a745', '#dc3545', '#ffc107']
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'bottom'
+                }
+            }
+        }
+    });
 });
