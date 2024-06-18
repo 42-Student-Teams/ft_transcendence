83ed7708cc076d4dd0184853145a375d25e63308
Arcadiastyx <i_naranjo@hotmail.com>
2024-06-18 18:15:02 +0200

    adding comments

--- django/run_socketio.py
+++ django/run_socketio.py
@@ -4,4 +4,4 @@ from socketio_server import app
 if __name__ == "__main__":
     import uvicorn
     print("Starting server on http://127.0.0.1:8069")
-    uvicorn.run(app, host="127.0.0.1", port=8069)
+    uvicorn.run(app, host="127.0.0.1", port=8069) # lance le serveur socket.io sur le port 8069
--- django/socketio_server.py
+++ django/socketio_server.py
@@ -6,10 +6,11 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 django.setup()
 
-sio = socketio.Server(async_mode='asgi')
-django_asgi_app = get_asgi_application()
-app = socketio.ASGIApp(sio, django_asgi_app)
+sio = socketio.Server(async_mode='asgi') # créé une instance de serveur socket.io
+django_asgi_app = get_asgi_application() # récupère l'application asgi de django
+app = socketio.ASGIApp(sio, django_asgi_app) # crée une application asgi avec le serveur socket.io et l'application django
 
+# définit les événements(connect, disconnect, message) qui seront écoutés par le serveur socket.io
 @sio.event
 def connect(sid, environ):
     print('Client connected:', sid)
