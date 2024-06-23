a267851a719f32ce0474cdcf37d732fc5b0f974a
sabaleonel <leonel@structo.ch>
2024-06-23 13:23:23 +0200

    added side chat and updated the navbar

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -1,16 +1,15 @@
 import NavBar from '../components/home/navbar.js';
+import SideChat from '../components/home/side-chat.js';
 import Component from "../library/component.js";
 
 export default class Home extends Component {
   constructor() {
     super({ element: document.getElementById("app") });
-
-    // store.events.subscribe("languageIdChange", () => this.renderAll());
-
     this.render();
 
     this.components = {
       navBar: new NavBar(),
+      sideChat: new SideChat(),
     };
   }
 
@@ -18,26 +17,36 @@ export default class Home extends Component {
     // const languageId = store.state.languageId;
 
     const view = /*html*/ `
-            <nav id="navBar">
-            </nav>
-            <div class="container-fluid">
-              <div class="row">
-                <div id="chat" class="sidebar col-md-2 d-none d-md-block bg-primary">
-                    <p>Hello</p>
-                </div>
-                <main class=" bg-secondary">
-                    <div class="w-100 d-flex justify-content-center align-items-center py-2">
-                      <h1 class="display-4 fw-bold">Game Mode Selection</h1>
+            <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark" id="navBar"></nav>
+            <div id="layoutSidenav">
+              <div id="layoutSidenav_nav"></div>
+              <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Static Navigation</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Static Navigation</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="mb-0">
+                                    This page is an example of using static navigation. By removing the
+                                    <code>.sb-nav-fixed</code>
+                                    class from the
+                                    <code>body</code>
+                                    , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
+                                </p>
+                            </div>
+                        </div>
+                        <div style="height: 100vh"></div>  
                     </div>
-                    <div class="d-flex flex-row gap-3 mt-3 justify-content-center">
-                          <div id="singleGameMode" class="" ></div>
-                          <div id="tournamentGameMode" class=" col"></div>
-                    </div>
-                  </main>
-                </div>
+                </main>
             </div>
-        `;
+        </div>
 
+        `;
     this.element.innerHTML = view;
+
   }
 }
