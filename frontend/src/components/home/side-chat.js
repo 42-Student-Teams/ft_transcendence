ca493530e4aa81118d92daaefc86041ddf72c174
sabaleonel <leonel@structo.ch>
2024-06-24 23:38:03 +0200

    added css for the chat but needs to be at the bottom

--- frontend/src/components/home/side-chat.js
+++ frontend/src/components/home/side-chat.js
@@ -10,18 +10,38 @@ export default class SideChat extends Component {
 
         const view = /*html*/ `
 
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading text-align">Chat</div>
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
                             <div class="container-fluid">
-                                <div></>
-                                
-                                
+                                <div class="d.none chat-screen">
+                                  <div class="header">
+                                    <div class="logo">Chatroom</div>
+                                  </div>
+                                  <div class="messages">
+                                    <div class="message my-message">
+                                      <div>
+                                        <div class="chat-name">You</div>
+                                        <div class="chat-text">Hi</div>
+                                      </div>
+                                    </div>
+                                    <div class="update">Abc joined the conversation</div>
+                                    <div class="message other-message">
+                                        <div>
+                                            <div class="chat-name">Abc</div>
+                                            <div class="chat-text">Hi</div>
+                                        </div>
+                                    </div>
+                                  </div>
+                                  <div class="typebox">
+                                    <input type="text" id="message-input" placeholder="Enter message" />
+                                    <button id="send-message"><i id="icon-send" class="fa-regular fa-paper-plane"></i></button>
+                                  </div>
+                                </div>
                             </div>
-                        </div>
                     </div>
-                </nav>
+                </div>
+            </nav>
         `;
 
         this.element = document.getElementById("layoutSidenav_nav");
