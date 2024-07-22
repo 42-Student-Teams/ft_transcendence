e79c01f02c9f07b398f1a3cd51ef363c3b97a143
Isabel Tovar <itovarnuez@gmail.com>
2024-07-22 12:13:11 +0200

    back up

--- frontend/src/views/localGame.js
+++ frontend/src/views/localGame.js
@@ -37,31 +37,31 @@ export default class LocalGame extends Component {
 			<canvas id="myCanvas"></canvas>
 
             </div>
-          </div>
-          <div class="col text-center">
+			</div>
+			<div class="col text-center">
             <h1 class="display-1">0</h1>
-          </div>
-        </div>
-      </div>
-    </div>
-        `;
-
-        this.element.innerHTML = view;
-        this.handleEvent();
-    }
-
-    async handleEvent() {
-
-		// JavaScript for bouncing ball and moving rectangle animation
-		const canvas = document.getElementById('myCanvas');
-		const ctx = canvas.getContext('2d');
+			</div>
+			</div>
+			</div>
+			</div>
+			`;
+			
+			this.element.innerHTML = view;
+			this.handleEvent();
+		}
+		
+		async handleEvent() {
+			
+			// JavaScript for bouncing ball and moving rectangle animation
+			const canvas = document.getElementById('myCanvas');
+			const ctx = canvas.getContext('2d');
  
 		// Initial ball properties
 		let x = canvas.width / 2;
 		let y = canvas.height / 2;
-		const ballRadius = 8;
+		const ballRadius = 3;
 		let isMoving = true; // Flag to check if the ball is moving
- 
+		
 		// Rectangle properties
 		let rectY1 = canvas.height / 2;
 		let rectY2 = canvas.height / 2;
@@ -78,9 +78,9 @@ export default class LocalGame extends Component {
 				dy: speed * Math.sin(angle)
 			};
 		}
-			
+		
 		let { dx, dy } = getRandomVelocity();
-			
+		
 		function drawBall() {
 			ctx.beginPath();
 			ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -119,37 +119,16 @@ export default class LocalGame extends Component {
 		
 		// Start the animation
 		let intervalId = setInterval(draw, 10); // Update every 10 milliseconds
+		// setInterval(dra	w, 10); // Update every 10 milliseconds
 		
 		// Event listener for keydown to stop the ball or move the rectangle
-		
-		
-		// document.addEventListener('keydown', (event) => {
-		//     if (event.code === 'Space') {
-		//         isMoving = !isMoving; // Toggle the movement flag
-		//         if (!isMoving) {
-		//             clearInterval(intervalId); // Stop the animation
-		//         } else {
-		//             intervalId = setInterval(draw, 10); // Restart the animation
-		//         }
-		//     }
-		//     // Move rectangle with W/S keys
-		// 	if (event.code === 'KeyW' && rectY1 > 0 ) {
-		// 		rectY1 -= rectSpeed; // Move up
-		// 	} else if (event.code === 'KeyS' && rectY1 < canvas.height - rectHeight) {
-		// 		rectY1 += rectSpeed; // Move down
-		// 	}
-		//     // Move rectangle with arrow keys
-		//     if (event.code === 'ArrowUp' && rectY2 > 0 ) {
-		//         rectY2 -= rectSpeed; // Move up
-		//     } else if (event.code === 'ArrowDown' && rectY2 < canvas.height - rectHeight) {
-		//         rectY2 += rectSpeed; // Move down
-		//     }
-		// });
 							
+
+
+		
 		let keysPressed = {};
 		document.addEventListener('keydown', (event) => {
 			keysPressed[event.code] = true;
-			
 			// if (keysPressed['Space']) {
 			//     isMoving = !isMoving; // Toggle the movement flag
 			//     if (!isMoving) {
@@ -159,24 +138,23 @@ export default class LocalGame extends Component {
 			//     }
 			// }
 			if (keysPressed['KeyW'] && rectY1 > 0 ) {
-				rectY1 -= rectSpeed; // Move up
+				rectY1 -= Math.min(rectY1, rectSpeed); // Move up
 			} 
 			if (keysPressed['KeyS'] && rectY1 < canvas.height - rectHeight) {
-				rectY1 += rectSpeed; // Move down
+				rectY1 += Math.min(canvas.height - rectY1, rectSpeed); // Move down
 			}
 			// Move rectangle with arrow keys
 			if (keysPressed['ArrowUp'] && rectY2 > 0 ) {
-				rectY2 -= rectSpeed; // Move up
+				rectY2 -= Math.min(rectY2, rectSpeed); // Move up
 			} 
 			if (keysPressed['ArrowDown']  && rectY2 < canvas.height - rectHeight) {
-				rectY2 += rectSpeed; // Move down
+				rectY2 += Math.min(canvas.height - rectY2, rectSpeed); // Move down
 			}
-					console.log(rectY1);
-			});
-			
-			document.addEventListener('keyup', (event) => {
-				delete keysPressed[event.code];
-			});
-			// mettre le jeu ici
+		});
+		
+		document.addEventListener('keyup', (event) => {
+			delete keysPressed[event.code];
+		});
+
 	}
 }
