737027da6707777a1fe1bf1a49151aa8671f01b4
jwikiera <a@a.com>
2024-08-02 15:23:46 +0200

    properly redirecting people to login if jwt invalid

--- frontend/src/components/register/form-register.js
+++ frontend/src/components/register/form-register.js
@@ -82,6 +82,8 @@ export default class FormRegister extends Component {
 
                     if (response.ok) {
                         store.dispatch("logIn");
+                        const jsonData = await response.json();
+                        localStorage.setItem('jwt', jsonData.jwt);
                         navigateTo("/");
                     } else {
                         showError('form-register', 'registration');
