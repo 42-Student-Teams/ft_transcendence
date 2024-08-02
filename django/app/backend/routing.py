33d5fe145c5ae39df028cade2993848ffc01577a
jwikiera <a@a.com>
2024-08-02 02:05:17 +0200

    removed testchat for good

--- django/app/backend/routing.py
+++ django/app/backend/routing.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from backend.consumers.testchat import ChatConsumer
 from backend.consumers.wsconsumer import WsConsumer
 
 websocket_urlpatterns = [
-    path('wss/chat/', ChatConsumer.as_asgi()),
     path('wss/comm/', WsConsumer.as_asgi()),
 ]
