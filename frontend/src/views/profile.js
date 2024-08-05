8cf62207ae05f11d079eed18992c28338b7921b2
Neila <neilamumi@hotmail.fr>
2024-08-05 12:56:13 +0200

    design change

--- frontend/src/views/profile.js
+++ frontend/src/views/profile.js
@@ -1,42 +1,37 @@
 import NavBar from '../components/home/navbar.js';
-import ChartComponent from "../components/profile/Chart.js";
 import MatchHistory from "../components/profile/MatchHistory.js";
 import ProfileInfo from "../components/profile/ProfileInfo.js";
-import Statistics from "../components/profile/Statistics.js";
 import Component from "../library/component.js";
 
 export default class Profile extends Component {
-	constructor() {
-		super({ element: document.getElementById("app") });
-		this.render();
+    constructor() {
+        super({ element: document.getElementById("app") });
+        this.render();
 
-		this.components = {
-			navBar: new NavBar(),
-			profileInfo: new ProfileInfo(),
-			chartComponent: new ChartComponent(),
-			statitics: new Statistics(),
-			matchHistory: new MatchHistory(),
-		};
-	}
+        this.components = {
+            navBar: new NavBar(),
+            profileInfo: new ProfileInfo(),
+            matchHistory: new MatchHistory(),
+        };
+    }
 
-	async render() {
-		const view = /*html*/ `
-		<div class="h-100 d-flex flex-column bg-custom vh-100">
-    		<div class="row m-0">
-    	    	<nav class="navbar navbar-expand bg-white shadow-sm w-100" id="navBar"></nav>
-    		</div>
-    		<div class="container-fluid p-0 row flex-fill overflow-hidden m-0">
-    	    	<div class="col-md-4 d-flex flex-column overflow-auto p-2">
-    	        	<div id="profileInfo" class="mb-2"></div>
-    	        	<div id="chart" class="flex-grow-1 d-flex flex-column"></div>
-    	    	</div>
-    	    	<div class="col-md-8 d-flex flex-column overflow-auto p-2">
-					<div id="statistics" class="mb-2"></div>
-    	        	<div id="matchHistory" class="flex-grow-1 d-flex flex-column"></div>
-    	   		</div>
-    		</div>
-		</div>
-
+    async render() {
+        const view = /*html*/ `
+        <div class="h-100 d-flex flex-column bg-custom vh-100">
+            <div class="row m-0">
+                <nav class="navbar navbar-expand bg-white shadow-sm w-100 mb-0" id="navBar"></nav>
+            </div>
+            <div class="container-fluid p-0 row flex-fill overflow-hidden m-0">
+                <div class="col-md-4 d-flex flex-column overflow-auto p-0">
+                    <!-- Ajouter des marges égales autour de ProfileInfo -->
+                    <div id="profileInfo" class="m-4"></div>
+                </div>
+                <div class="col-md-8 d-flex flex-column overflow-auto p-0">
+                    <!-- Ajouter des marges en haut, à droite et en bas pour MatchHistory -->
+                    <div id="matchHistory" class="flex-grow-1 d-flex flex-column mt-4 me-4 mb-4"></div>
+                </div>
+            </div>
+        </div>
         <!-- Modal Edit Profile -->
         <div class="modal" id="edit-profile-modal" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
@@ -52,12 +47,12 @@ export default class Profile extends Component {
                                 <input type="file" class="form-control" id="edit-profile-picture">
                             </div>
                             <div class="p-3">
-                                <label for="edit-username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="edit-username" required>
+                                <label for="edit-first-name" class="form-label">First Name</label>
+                                <input type="text" class="form-control" id="edit-first-name" required>
                             </div>
                             <div class="p-3">
-                                <label for="edit-email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="edit-email" required>
+                                <label for="edit-last-name" class="form-label">Last Name</label>
+                                <input type="text" class="form-control" id="edit-last-name" required>
                             </div>
                         </div>
                         <div class="modal-footer">
@@ -69,29 +64,27 @@ export default class Profile extends Component {
             </div>
         </div>
         `;
+        this.element.innerHTML = view;
+        this.handleEvent();
+    }
 
-		this.element.innerHTML = view;
-		this.handleEvent();
-	}
+    async handleEvent() {
+        document.getElementById("edit-profile-form").addEventListener("submit", async (event) => {
+            event.preventDefault();
 
-	async handleEvent() {
-		document.getElementById("edit-profile-form").addEventListener("submit", async (event) => {
-			event.preventDefault();
+            const profilePicture = document.getElementById("edit-profile-picture").files[0];
+            const firstName = document.getElementById("edit-first-name").value;
+            const lastName = document.getElementById("edit-last-name").value;
 
-			const profilePicture = document.getElementById("edit-profile-picture").files[0];
-			const username = document.getElementById("edit-username").value;
-			const email = document.getElementById("edit-email").value;
+            // Store values here
+            const profileData = {
+                profilePicture,
+                firstName,
+                lastName,
+            };
 
-			// valeurs stockées ici
-			const profileData = {
-				profilePicture,
-				firstName,
-				lastName,
-				username,
-				email,
-				password
-			};
-			//console.log(profileData);
-		});
-	}
+            // Here you can handle the form submission, like sending the data to the server
+            console.log(profileData);
+        });
+    }
 }
